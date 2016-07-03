9859840e857f323da7d28fd31b5cbba0976d941b
Thomas Fricke <thomas@endocode.com>
2016-07-03 22:57:41 +0200

    added topology as separate header

---
+++ topology.h
@@ -0,0 +1,23 @@
+#ifndef __TOPOLOGY_H__
+#define __TOPOLOGY_H__
+
+#include <model.h>
+extern size_t number_of_cells;
+extern cell * cells;
+
+inline cell * random_neighbour(cell * source) {
+/* draw dest cell, right or left neighbour */
+      cell * dest;
+      if(rand55() & 1){ /* bad example */
+        dest=source+1;
+        if(dest==cells+number_of_cells) /* cyclic boundaries: right out, left in */
+          dest=cells;
+      }
+      else{
+        dest=source-1;
+        if(dest<cells) /* cyclic boundaries: left out, right in */
+          dest+=number_of_cells;
+      }
+      return dest;
+}
+#endif 
